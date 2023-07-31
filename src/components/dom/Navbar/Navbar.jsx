810860e7cdf55a3e1227a1cc4ee630c8d94f7298
Marat Musin <maratmusin@MBP-Marat.Dlink>
2023-07-31 17:51:06 +0300

    refact:navbar position fixed

--- src/components/dom/Navbar/Navbar.jsx
+++ src/components/dom/Navbar/Navbar.jsx
@@ -12,20 +12,19 @@ import LocaleSwitcher from './LocaleSwitcher'
 export default function Navbar() {
   const t = useTranslations('Navigation')
   return (
-    <nav className={cls['container']}>
+    <nav className={cls['container']} >
       <div className={`${cls['nav-section']} ${cls['nav-logo-section']} `}>
         {/* <a href='/'>marchitecht </a> */}
         {/* <div className=' text-center h-10 w-10 rounded-full bg-white/90 p-1 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10'> */}
-          <a href='/'>
-            <img className='w-10 h-10 p-1 rounded-full ring-2 ring-gray-600' src='/marchitecht.jpg' alt='' />
-          </a>
+        <a href='/'>
+          <img className='w-10 h-10 p-1 rounded-full ring-2 ring-gray-600' src='/marchitecht.jpg' alt='' />
+        </a>
         {/* </div> */}
       </div>
-     
+
       <div className={cls['nav-mobile-section']}>
         <div className={`${cls['nav-section']} ${cls['nav-link-section']} `}>
           <a href='/about'>{t('About')}</a>
-          {/* <a href='#'>BLOG</a> */}
         </div>
         <div className={`${cls['nav-language-section']} ${cls['nav-section']} `}>
           <LocaleSwitcher />
