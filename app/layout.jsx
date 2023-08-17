e8c962d3fbd7f01878d26dd94beb36cd4c2fed78
Marat Musin <maratmusin@MBP-Marat.Dlink>
2023-08-17 08:52:01 +0300

    feat:added vercel-analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,13 @@
 import { ReactNode, Suspense } from 'react'
 import './global.css'
-
+import { Analytics } from '@vercel/analytics/react'
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }) {
   return (
     <>
-    
+      <Analytics />
       <body>{children}</body>
     </>
   )
